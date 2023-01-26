2480eb9747a6a53b516c728320a01bbfd0eab2e7
Erick Michel <michel172002@gmail.com>
2023-01-25 21:45:06 -0300

    Criado Microservice de envio de Email.

---
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,27 @@
+package com.ms.email.models;
+
+import com.ms.email.enums.StatusEmail;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+//@Data é do lombok para nao colocar get, set, construtores
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDataEmail;
+    private StatusEmail statusEmail;
+}
